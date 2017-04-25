a3daba419c128b8ed420c91439644c0026ddf2c3
rafaelkallis <rk@rafaelkallis.com>
2017-04-25 14:29:07 +0200

    4.2

--- Uebung04/main.py
+++ Uebung04/main.py
@@ -6,8 +6,13 @@ from lxml import etree
 import json
 from functools import reduce
 import heapq
+import bz2
+import urllib.request
+import random
+from tempfile import TemporaryFile
 
 
+# tested and works!
 def getfreqwords(indir, outfile):
     cache = {}
     for file in glob(indir + "/SAC-Jahrbuch_*_mul.xml"):
@@ -35,5 +40,43 @@ def getfreqwords(indir, outfile):
                 if hash in top20hash:
                     top20sentences.add(" ".join(tokens))
 
-    f = open(outfile, 'w')
-    f.write('\n'.join(top20sentences))
+    outfile.write('\n'.join(top20sentences))
+
+
+# not tested! testfile in a+ mode and trainfile in a mode
+def gettitles(infile, testfile, trainfile, k):
+    # don't know how the file looks, potential wrong read
+    tree = etree.iterparse(StringIO(infile))
+
+    # reservoir sampling
+    t = 0
+    for action, elem in tree:
+        if t < k:
+            # appends elem to testfile
+            testfile.write(elem)
+        else:
+            m = random.randint(0, t)
+            if m < k:
+                # temp = testfile;
+                # trainfile.append(temp[m]);
+                # temp[m] = elem;
+                # testfile.clear()
+                # testfile = temp
+                temp = TemporaryFile("a")
+
+                for idx, line in enumerate(testfile):
+                    if m == idx:
+                        temp.write(elem)
+                        trainfile.write(line)
+                    else:
+                        temp.write(line)
+
+                # clear testfile
+                testfile.seek(0)
+                testfile.truncate()
+
+                # testfile = temp
+                for line in temp:
+                    testfile.write(line)
+        t += 1
+    return reservoir
